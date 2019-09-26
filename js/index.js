982fdbcc54a165d4256a37bfe9070d9018181839
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-09-26 16:36:50 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function getRepositories() {
   const req = new XMLHttpRequest();
   let username = document.querySelector('')
   req.addEventListener('load', displayRepositories);
-  req.open('GET', 'https://api.github.com/users/' + octocat '/repos');
+  req.open('GET', 'https://api.github.com/users/' + username '/repos');
   req.send();
 }
 
